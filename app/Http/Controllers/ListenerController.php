6ad6230e5ae02e84e148543d9405b23eacb33360
CarlosCortezRubio <presto_ccr@hotmail.com>
2022-01-10 11:27:41 -0500

    --

--- app/Http/Controllers/ListenerController.php
+++ app/Http/Controllers/ListenerController.php
@@ -102,12 +102,12 @@ class ListenerController extends Controller
          $user->save();
       }
 
-     // $enlace = config('app.url_admision');
+      $enlace = config('app.url_admision');
 
       if($modalidad == 'E'){
         $this->enviarMensajeExonerado($email);
       } else {
-     //   $this->enviarMensaje($enlace, $email, $contrasena);
+        $this->enviarMensaje($enlace, $email, $contrasena);
       }
 	  
       return true;
